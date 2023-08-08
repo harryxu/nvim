54b40fa92d0346fd8a923b9863a09021c9b60b1e
harryxu <harryzhxu@gmail.com>
2023-08-09 03:28:01 +0800

    cmp-look

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,43 +37,52 @@ return {
 
   -- auto completion
   {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
     version = false, -- last release is way too old
-    event = "InsertEnter",
+    event = 'InsertEnter',
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'octaltree/cmp-look',
     },
     opts = function()
-      -- vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
+      -- vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
+      local cmp = require('cmp')
+      local defaults = require('cmp.config.default')()
       return {
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = 'menu,menuone,noinsert',
         },
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<S-CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+          {
+            name = 'look',
+            keyword_length = 3,
+            option = {
+                convert_case = true,
+                loud = true
+            }
+          }
         }),
         sorting = defaults.sorting,
       }
@@ -83,7 +92,7 @@ return {
   -- autopairs
   {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
+    event = 'InsertEnter',
     opts = {} -- this is equalent to setup({}) function
   },
 
