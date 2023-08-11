c14a389a7fff2e6e01a1f9dc867b5c3214d93205
harryxu <harryzhxu@gmail.com>
2023-08-12 02:42:03 +0800

    Better config for nvim-ufo.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,10 +36,11 @@ vim.opt.smartcase = true
 --vim.opt.autochdir = true -- switch to the directory of the open buffer
 
 -- fold settings
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 vim.opt.wildmenu = true
 vim.opt.completeopt = 'longest,menuone'
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,11 +1,35 @@
 return  {
+
+  -- UFO folding
+  -- Copied from https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-1512772530
   {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { 'kevinhwang91/promise-async' },
-    config = function()
-      require('ufo').setup()
-    end,
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+    event = "BufReadPost",
+    opts = {
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+    },
+
   },
+
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
